5a3c4e7abc568509a84c9196dc9e04a47aaa4d77
Jan Svacina <jan_svacina2@baylor.edu>
2018-11-05 18:27:42 -0600

    writing to output dir

--- gw-similarity/src/main/java/similarity/service/SimilarityService.java
+++ gw-similarity/src/main/java/similarity/service/SimilarityService.java
@@ -1,8 +1,14 @@
 package similarity.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import similarity.domain.HarvesterData;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,18 +17,34 @@ import java.util.ArrayList;
 @Service
 public class SimilarityService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public void compareModules(){
         //get data
         ArrayList<HarvesterData> harvesterDataArrayList = new ArrayList<>();
 
-        int counter = 0;
-        File file = new File("/Users/svacina/Dev/01Research/centralized-prespective/global-weaver/similarity-detection/output"+"module"+);
-        file.getParentFile().mkdirs();
-        try {
-            FileWriter writer = new FileWriter(file);
-        } catch (IOException e) {
-            e.printStackTrace();
+        ResponseEntity<ArrayList<HarvesterData>> response = restTemplate.exchange(
+                "http://localhost:7003/security",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<ArrayList<HarvesterData>>(){});
+        ArrayList<HarvesterData> data = response.getBody();
+
+        for (int i = 0; i < data.size(); i++){
+            File file = new File("/Users/svacina/Dev/01Research/centralized-prespective/global-weaver/similarity-detection/output/"+"module"+i+"/Similarity"+i+".java");
+            file.getParentFile().mkdirs();
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
+
 
         //create folder + file for each data input + insert data
 
