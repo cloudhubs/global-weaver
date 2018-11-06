124f0c20085fc94e725c965e29e439f7e039b231
Jan Svacina <jan_svacina2@baylor.edu>
2018-11-05 20:57:34 -0600

    harvester logging

--- gw-harvester/src/main/java/harvester/aspect/HarvesterLoggingAspect.java
+++ gw-harvester/src/main/java/harvester/aspect/HarvesterLoggingAspect.java
@@ -1,10 +1,13 @@
 package harvester.aspect;
 
+import harvester.controller.HarvesterController;
 import harvester.domain.HarvesterData;
 import harvester.domain.LocalWeaverResult;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -13,9 +16,9 @@ import org.springframework.context.annotation.Configuration;
 @Aspect
 @Configuration
 @Configurable
-public class HarvesterServiceAspect {
+public class HarvesterLoggingAspect {
 
-    private Logger logger = LoggerFactory.getLogger(HarvesterServiceAspect.class);
+    private Logger logger = LoggerFactory.getLogger(HarvesterLoggingAspect.class);
 
     @Around(value="harvester.aspect.CommonJoinPointConfig.harvesterServiceGetData()")
     public HarvesterData getDataAspectMethod(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -28,4 +31,11 @@ public class HarvesterServiceAspect {
         }
         return harvesterData;
     }
+
+    @Before(value="harvester.aspect.CommonJoinPointConfig.harvesterControllerMethods()")
+    public void logHarvesterController(JoinPoint joinPoint){
+        logger = LoggerFactory.getLogger(HarvesterController.class);
+        logger.info(joinPoint.getSignature().getName() + "method call");
+    }
+
 }
