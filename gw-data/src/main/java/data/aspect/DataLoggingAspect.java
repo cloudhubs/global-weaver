4df0d6fce6b85e5879c8d050659700b95a0dfaf8
Jan Svacina <jan_svacina2@baylor.edu>
2018-11-07 17:44:56 -0600

    data aspects working

--- gw-data/src/main/java/data/aspect/DataLoggingAspect.java
+++ gw-data/src/main/java/data/aspect/DataLoggingAspect.java
@@ -2,9 +2,11 @@ package data.aspect;
 
 import data.domain.HarvesterData;
 import data.domain.LocalWeaverResult;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -17,17 +19,9 @@ public class DataLoggingAspect {
 
     private Logger logger = LoggerFactory.getLogger(DataLoggingAspect.class);
 
-    @Around(value="data.aspect.CommonJoinPointConfig.dataServiceGetData()")
-    public HarvesterData getDataAspectMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType());
-        HarvesterData harvesterData = (HarvesterData) joinPoint.proceed();
-        if(harvesterData.getData().size() > 0) {
-            for (LocalWeaverResult lr : harvesterData.getData()) {
-                logger.info("Getting data from local weaver: " + lr.getModuleName());
-            }
-        } else {
-            logger.info("No modules detected");
-        }
-        return harvesterData;
+    @Before(value="data.aspect.CommonJoinPointConfig.dataServiceGetData()")
+    public void getDataAspectMethod(JoinPoint joinPoint) throws Throwable {
+
+        System.out.printf("ahoj");
     }
 }
