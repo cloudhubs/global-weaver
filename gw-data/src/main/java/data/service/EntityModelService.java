3e675db71518638f7514923f399f859ccde5448e
Jan Svacina <jan_svacina2@baylor.edu>
2019-02-28 23:25:59 -0600

    generating fields in graph

--- gw-data/src/main/java/data/service/EntityModelService.java
+++ gw-data/src/main/java/data/service/EntityModelService.java
@@ -1,8 +1,14 @@
 package data.service;
 
+import edu.baylor.ecs.seer.common.Pair;
 import edu.baylor.ecs.seer.common.context.SeerContext;
 import edu.baylor.ecs.seer.common.context.SeerMsContext;
+import edu.baylor.ecs.seer.common.entity.EntityModel;
+import edu.baylor.ecs.seer.common.entity.SeerField;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EntityModelService {
@@ -14,7 +20,7 @@ public class EntityModelService {
              ) {
             if (ms.getEntity().getEntities() != null && ms.getEntity().getEntities().size() > 0){
                 start();
-                body();
+                body(ms.getEntity().getEntities());
                 end();
                 ms.getEntity().setBoundedContextSource(stringBuilder.toString());
             }
@@ -39,45 +45,134 @@ public class EntityModelService {
                 "        edge [\n" +
                 "                fontname = \"Bitstream Vera Sans\"\n" +
                 "                fontsize = 8\n" +
-                "        ]");
+                "        ]" +
+                " \n"+
+                " \n"
+        );
     }
 
-    private void body(){
-        stringBuilder.append("Team [\n" +
-                "                label = \"{Team|+ name : string\\l+ code : int\\l|+ die() : void\\l}\"\n" +
-                "        ]\n" +
-                " \n" +
-                "        subgraph clusterContestImpl {\n" +
-                "                label = \"Package contest.impl\"\n" +
-                " \n" +
-                "                Member [\n" +
-                "                        label = \"{Member||+ login() : void\\l}\"\n" +
-                "                ]\n" +
-                " \n" +
-                "                Contest [\n" +
-                "                        label = \"{Contest||+ start() : void\\l}\"\n" +
-                "                ]\n" +
-                "        }\n" +
-                " \n" +
-                "        edge [\n" +
-                "                arrowhead = \"empty\"\n" +
-                "        ]\n" +
-                " \n" +
-                "        Member -> Team\n" +
-                "        Contest -> Team\n" +
+    /**
+     * edge [
+     *                 arrowhead = "none"
+     *
+     *                 headlabel = "0..*"
+     *                 taillabel = "0..*"
+     *         ]
+     * @param listEntitiesModel
+     */
+    private void body(List<EntityModel> listEntitiesModel){
+
+        for (EntityModel em: listEntitiesModel
+             ) {
+            generateEntity(em);
+        }
+
+        stringBuilder.append("" +
+//                        "Team [\n" +
+//                "                label = \"{Team|+ name : string\\l+ code : int\\l|+ die() : void\\l}\"\n" +
+//                "        ]\n" +
+//                " \n" +
+//                "        subgraph clusterContestImpl {\n" +
+//                "                label = \"Package contest.impl\"\n" +
+//                " \n" +
+//                "                Member [\n" +
+//                "                        label = \"{Member||+ login() : void\\l}\"\n" +
+//                "                ]\n" +
+//                " \n" +
+//                "                Contest [\n" +
+//                "                        label = \"{Contest||+ start() : void\\l}\"\n" +
+//                "                ]\n" +
+//                "        }\n" +
                 " \n" +
                 "        edge [\n" +
                 "                arrowhead = \"none\"\n" +
-                " \n" +
-                "                headlabel = \"0..*\"\n" +
-                "                taillabel = \"0..*\"\n" +
+                "                 headlabel = \"0..*\" " +
+                "                 taillabel = \"0..*\" " +
                 "        ]\n" +
                 " \n" +
-                "        Member -> Contest");
+               // "        Member -> Team\n" +
+               // "        Contest -> Team\n" +
+                " \n" +
+              //  "        edge [\n" +
+              //  "                arrowhead = \"none\"\n" +
+              //  " \n" +
+              //  "                headlabel = \"0..*\"\n" +
+              //  "                taillabel = \"0..*\"\n" +
+              //  "        ]\n" +
+                " \n" //+
+               // "        Member -> Contest"
+        );
+
+        for (EntityModel em: listEntitiesModel
+        ) {
+            generateEdge(em);
+        }
+
     }
 
     private void end(){
         stringBuilder.append("}");
     }
+
+    private void generateEntity(EntityModel entityModel){
+        stringBuilder.append(entityModel.getClassNameShort());
+        stringBuilder.append("[\n" +
+                " label = \"{");
+        stringBuilder.append(entityModel.getClassNameShort());
+        stringBuilder.append("|");
+        for (SeerField seerField: entityModel.getFields()
+             ) {
+            String type = seerField.getType();
+            String name = seerField.getName();
+            String fieldString = String.format("+ %1$s : %2$s\\l", name, type);
+            stringBuilder.append(fieldString);
+        }
+//        stringBuilder.append("+ type : string\\l");
+//        stringBuilder.append("+ name : string\\l");
+        // methods
+        stringBuilder.append(" \n");
+
+        //stringBuilder.append("|+ login() : void\\l");
+                        stringBuilder.append(
+                        "}\"\n" +
+                "  ]\n");
+        stringBuilder.append(" \n");
+        stringBuilder.append(" \n");
+        stringBuilder.append(" \n");
+    }
+
+    private void generateEdge(EntityModel entityModel){
+
+        List<String> names = new ArrayList<>();
+        List<String> types = new ArrayList<>();
+
+        for (SeerField seerField: entityModel.getFields()
+        ) {
+
+        }
+
+        stringBuilder.append("\n");
+
+        for (SeerField seerField: entityModel.getFields()
+             ) {
+            if (seerField.getSeerEntityRelation() != null){
+
+                stringBuilder.append(" \n" +
+                        "        edge [\n" +
+                        "                arrowhead = \"none\"\n" +
+                        "                 headlabel = \"0..*\" " +
+                        "                 taillabel = \"0..*\" " +
+                        "        ]\n" +
+                        " \n");
+
+                stringBuilder.append("\n");
+                stringBuilder.append("" + entityModel.getClassNameShort() + " -> " + seerField.getType());
+                stringBuilder.append("\n");
+            }
+        }
+
+        stringBuilder.append("\n");
+    }
+
 
 }
